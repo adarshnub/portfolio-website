caf45c11b9e53f26554a6d80a9d5c6ac3b3af887
adarshnub <adarshmanjady@gmail.com>
2023-01-12 22:10:46 +0530

    navbar complete

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {HiArrowNarrowRight} from "react-icons/hi";
+
+
+
+const Home = () => {
+  return ( 
+    <div className="w-full h-screen bg-[#0a192f]">
+
+       
+        
+        {/*container*/}
+        <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full pl-24 ">
+            <p className="text-red-400">Hi, my name is</p>
+            <div class="typewriter">
+            <h1 className="text-4xl sm:text-7xl text-gray-400 font-bold">V Adarsh</h1>
+            </div>
+            <h2 className="text-4xl sm:text-6xl text-gray-700 font-bold py-4">I'm a Frontend developer</h2>
+            <p className="text-gray-700 py-4 max-w-[700px]">I'm a Frontend developer specializing in building and constantly
+                designing exceptional React websites. Currently, i'm focused on
+                building responsive react applications
+            </p>
+            <div>
+                <a href="/" className="relative inline-flex items-center  justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">View Works</span>
+<span className="relative invisible">View Works</span>
+</a>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React ,{useState} from 'react';
+import logo from "../assets/logo.png";
+import {FaBars ,FaTimes ,FaGithub ,FaLinkedin} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi";
+import { BsFillPersonLinesFill} from "react-icons/bs";
+
+
+
+
+const Navbar = () => {
+    const [nav ,setNav] = useState(false);
+    const handleClick = () => {
+        setNav(!nav)
+    }
+
+  return (
+    <div className="fixed w-full h-[80px] text-gray-300 flex justify-between items-center px-4 bg-[#0a192f]">
+        <div>
+            <img src={logo} alt="logo " className="w-[50px]" />
+        </div>
+
+        {/*menu*/}
+        
+            <ul className="hidden md:flex text-purple-500">
+                <li><a href="#_" class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-[#0a192f] rounded-3xl group">
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-purple-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#0a192f]"></span>
+                    <span class="relative">Home</span>
+                    </a>
+                </li>
+                <li>
+                <a href="#_" class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-[#0a192f] rounded-3xl group">
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-purple-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#0a192f]"></span>
+                    <span class="relative">About</span>
+                    </a>
+                </li>
+                <li>
+                <a href="#_" class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-[#0a192f] rounded-3xl group">
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-purple-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#0a192f]"></span>
+                    <span class="relative">Skills</span>
+                    </a>
+                </li>
+                <li>
+                <a href="#_" class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-[#0a192f] rounded-3xl group">
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-purple-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#0a192f]"></span>
+                    <span class="relative">Works</span>
+                    </a>
+                </li>
+                <li>
+                <a href="#_" class="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-[#0a192f] rounded-3xl group">
+                    <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-purple-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+                    <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#0a192f]"></span>
+                    <span class="relative">Contact</span>
+                    </a>
+                </li>
+            </ul>
+        
+
+        {/*hamburger icon*/}
+        <div onClick={handleClick} className="md:hidden z-30 cursor-pointer">
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/*Mobile menu*/}
+        <ul className={!nav ? "hidden" : "absolute  top-0 left-0 w-full h-screen z-20 bg-[#0a192f] flex flex-col justify-center items-center lg:hidden"}>
+                <li className="py-6 text-4xl hover:font-bold hover:text-purple-500">Home</li>
+                <li className="py-6 text-4xl hover:font-bold hover:text-purple-500">About</li>
+                <li className="py-6 text-4xl hover:font-bold hover:text-purple-500">Skills</li>
+                <li className="py-6 text-4xl hover:font-bold hover:text-purple-500">Works</li>
+                <li className="py-6 text-4xl hover:font-bold hover:text-purple-500">Contact</li>
+        </ul>
+
+
+        {/*social icons*/}
+        <div className="hidden md:flex flex-col fixed top-[35%] left-0">
+            <ul>
+                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
+                    <a className="flex justify-between items-center w-full text-gray-300 font-semibold" 
+                    href="https://www.linkedin.com/in/adarsh-viswam-95161016b">
+                        Linkedin <FaLinkedin size={30} /> 
+                    </a>
+                </li>
+                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800">
+                    <a className="flex justify-between items-center w-full text-gray-300 font-semibold" 
+                    href="https://github.com/adarshnub">
+                        Github <FaGithub size={30} /> 
+                    </a>
+                </li>
+                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-300">
+                    <a className="flex justify-between items-center w-full text-white font-semibold" 
+                    href="/">
+                        Email <HiOutlineMail size={30} /> 
+                    </a>
+                </li>
+                <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-300">
+                    <a className="flex justify-between items-center w-full text-white font-semibold" 
+                    href="/">
+                        Resume <BsFillPersonLinesFill size={30}  /> 
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+  );
+}
+
+export default Navbar;
