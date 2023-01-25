7ee02dcc454e723faed94f7413e9b335a90f5fdd
adarshnub <adarshmanjady@gmail.com>
2023-01-25 19:02:49 +0530

    updates commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   );
 }
